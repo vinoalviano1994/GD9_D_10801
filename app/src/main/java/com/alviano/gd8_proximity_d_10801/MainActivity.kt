f28b7242e855ccaef88df1b16217867dd5cba062
Dyonisius Alviano M E <vinoalviano1994@gmail.com>
2022-11-03 09:07:40 +0700

    proximity

---
+++ app/src/main/java/com/alviano/gd8_proximity_d_10801/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.alviano.gd8_proximity_d_10801
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var sensorStatusTV: TextView
+    lateinit var proximitySensor: Sensor
+    lateinit var sensorManager: SensorManager
+
+    var proximitySensorEventListener: SensorEventListener? = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+        }
+
+        override fun onSensorChanged(event: SensorEvent) {
+            if(event.sensor.type == Sensor.TYPE_PROXIMITY){
+                if(event.values[0] == 0f){
+                    sensorStatusTV.text="<<<Near>>>"
+                }else{
+                    sensorStatusTV.text="<<<Away>>>"
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sensorStatusTV = findViewById(R.id.idTVSensorStatus)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+
+        if(proximitySensor == null) {
+            Toast.makeText(this,"No Proximity sensor found in device..",Toast.LENGTH_SHORT).show()
+            finish()
+        }else{
+            sensorManager.registerListener(
+                proximitySensorEventListener,
+                proximitySensor,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
+    }
+
+
+}
